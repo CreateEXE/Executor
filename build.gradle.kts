e1112d7740b804ec0fdb14620af65aa5ad0628c9
CreateEXE <Creator.EXE@Outlook.com>
2026-05-19 21:41:26 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.android.application") version "8.6.0" apply false
-    id("com.android.library") version "8.6.0" apply false
-    kotlin("android") version "2.0.0" apply false
-    kotlin("plugin.serialization") version "2.0.0" apply false
+    id("com.android.application") version "8.7.2" apply false
+    id("com.android.library") version "8.7.2" apply false
+    kotlin("android") version "2.1.0" apply false
+    kotlin("plugin.serialization") version "2.1.0" apply false
 }
 
 tasks.register("clean", Delete::class) {
